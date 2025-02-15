091598e9f80b4b8f2d92d69695096be34a067a93
nzalpha <148647509+nzalpha@users.noreply.github.com>
2025-02-14 21:09:33 -0800

    testing add logic

---
+++ src/com/smartapp/builds/Docker.groovy
@@ -0,0 +1,24 @@
+package com.smartapp.builds
+
+class Docker{
+    def jenkins
+    Docker(jenkins){
+        this.jenkins =jenkins
+    }
+
+    // Test Addition method
+
+    def add(fNum,sNum){
+        return fNum + sNum
+    }
+
+    // Method for Application Build
+
+    // Docker build
+
+    // Docker login
+
+
+
+
+}
---
+++ vars/dockerPipeline.groovy
@@ -0,0 +1,25 @@
+
+import com.smartapp.builds.Docker
+
+def call(Map pipelineParams){
+    Docker d = new Docker(this)
+
+    pipeline{
+    agent{
+        label 'workernode-1'
+    }
+
+    stages{
+        stage("Adding"){
+            steps{
+                script{
+                    echo "--------------------- Executing Add Method ----------------------"
+                    printlin d.add(4,5)
+                }
+            }
+        }
+    }
+
+    }
+
+}

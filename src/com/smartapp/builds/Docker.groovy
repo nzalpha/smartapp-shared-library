aa7f633711fbd466f0dc60d0e901090144b9b5de
nzalpha <148647509+nzalpha@users.noreply.github.com>
2025-02-15 15:16:14 -0800

    comments removed

--- src/com/smartapp/builds/Docker.groovy
+++ src/com/smartapp/builds/Docker.groovy
@@ -30,7 +30,6 @@ class Docker{
         echo "Entering into K8 Auth/Login Method"
         gcloud compute instances list
         gcloud container clusters get-credentials $gke_cluster_name --region $gke_region --project smart-k8
-       // gcloud container clusters get-credentials cluster-2 --region us-central1 --project smart-k8
         Kubectl get nodes
         """
     }
